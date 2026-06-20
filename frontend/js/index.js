c1398874856dd468efe99cf73e25ee1961031c15
24wh1a0502 <24wh1a0502@bvrithyderabad.edu.in>
2026-06-20 10:37:13 +0530

    correct arndom user

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,2 +1,29 @@
-alert("hello from JS : !");
-console.log("hello from js!");
+var users = [
+    {
+        "name": "John doe",
+        "gender": "male",
+        "images": "/images/john.png"
+    },
+    {
+        "name": "sriharsha",
+        "gender": "female",
+        "images": "/images/jane.png"
+    }
+];
+
+var curId = 0;
+
+function render() {
+    var user = users[curId];
+    document.getElementById("user-img").src = user.images;
+    document.getElementById("user-name").innerText = user.name;
+    document.getElementById("user-gender").innerText = user.gender;
+}
+
+function toggle() {
+    curId = (curId + 1) % 2;
+    render();
+}
+
+// Show the first user when the page loads
+render();
